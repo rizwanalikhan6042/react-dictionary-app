b8bd0f7463e71d64b3a99ff6c0ab6b8509613804
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2025-08-04 17:18:02 +0530

    initial commit

---
+++ src/components/Dictionary.js
@@ -0,0 +1,31 @@
+import React from "react";
+import useDictionary from "../hooks/useDictionary";
+
+function Dictionary() {
+    const {
+        word, setWord, meaning, setMeaning, isLoading,
+        handleSearch, definition, setResult, result, setDefinition
+    } = useDictionary();
+    return (
+        <div className="dictionary-container">
+            <input type="text" value={word} onChange={(e) => setWord(e.target.value)}
+                placeholder="Enter a word" onKeyDown={(e) => {
+                    if (e.key === "Enter") handleSearch(e);
+                }} />
+            <button onClick={handleSearch} disabled={isLoading}>
+                {isLoading ? "searching" : "search"}
+            </button>
+            {definition && (
+                <div className="definition-box">
+                    <h2>Definition:</h2>
+                    <p>{definition}</p>
+                </div>
+            )}
+        </div>
+    )
+
+
+}
+
+export default Dictionary;
+
---
+++ src/hooks/useDictionary.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+
+function useDictionary() {
+    const [word, setWord] = useState("");
+    const [meaning, setMeaning] = useState("");
+    const [definition, setDefinition] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [result, setResult] = useState("");
+
+
+    const handleSearch = (e) => {
+        e.preventDefault(); 
+        fetchMeaning();    
+    };
+
+    const fetchMeaning = async () => {
+        if (!word.trim()) {
+            return;        // Skip if input is empty
+        }
+        setIsLoading(true);
+        setError(null);
+        try {
+            const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+            const data = await res.json();
+            setDefinition(data[0]?.meanings[0]?.definitions[0]?.definition || "No defintiton found.")
+            setIsLoading(false)
+            console.log("API data :", data);
+            setResult(data);
+        }
+        catch (error) {
+            setError("Failed to fetch meaning.");
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+    return {
+        word,
+        setWord,
+        definition,
+        isLoading,
+        handleSearch
+    };
+
+}
+
+export default useDictionary
